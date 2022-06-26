50c8e1bd0da96b1eefe413cab94ba50c65d1cc96
ElectroPerf <kunmun.devroms@gmail.com>
2022-06-26 07:12:06 +0000

    sdm660-common: Build Some Required Bluetooth Packages

--- sdm660.mk
+++ sdm660.mk
@@ -77,8 +77,11 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
