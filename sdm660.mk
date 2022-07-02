95e3b33e973c472629210712bd9c4342248a9330
Pranav Kapoor <pranavkapoor001@gmail.com>
2022-07-02 11:46:17 +0000

    sdm660-common: Build `android.hardware.light`

--- sdm660.mk
+++ sdm660.mk
@@ -261,7 +261,8 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     lights.qcom \
-    android.hardware.lights-service.qti
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Media
 PRODUCT_PACKAGES += \
